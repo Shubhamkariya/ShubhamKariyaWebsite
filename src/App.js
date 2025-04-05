0ca6a7d13f8443d397dd613ffa7cb2ce30916c58
Shubham Kariya <129506067+Shubhamkariya@users.noreply.github.com>
2025-04-06 01:38:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,35 +3,8 @@ import Main from "./containers/Main";
 import React, { useEffect } from 'react';
 import ReactGA from 'react-ga4';
 
-// Import your GTM_ID from the constants file
-import { GTM_ID } from './constants'; // Assuming constants.js is in the same directory
-// If constants.js is in a different location, adjust the path accordingly
-// For example: import { GTM_ID } from './utils/constants';
-interface Window {
-  dataLayer: any[];
-}
 
 const App = () => {
-  useEffect(() => {
-    // Initialize Google Tag Manager
-    const script = document.createElement('script');
-    script.src = `https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`;
-    script.async = true;
-    document.head.appendChild(script);
-
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', GTM_ID);
-
-    // Optional: Clean up the script tag on unmount
-    return () => {
-      const gtmScript = document.querySelector(`script[src='https://www.googletagmanager.com/gtag/js?id=${GTM_ID}']`);
-      if (gtmScript) {
-        document.head.removeChild(gtmScript);
-      }
-    };
-  }, []);
 
   return (
     <div>
