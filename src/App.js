78fe27e6bb7d15628ac24a0e2ef0ef9a2ba2c336
Shubham Kariya <129506067+Shubhamkariya@users.noreply.github.com>
2025-04-06 01:06:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import "./App.scss";
 import Main from "./containers/Main";
 import React from 'react';
 import ReactGA from 'react-ga4'; 
+import { useEffect } from 'react';
 
 
 const App = () => {
-  ReactGA.initialize('GTM-KM7WDCDN'); 
+   useEffect(() => {
+    const script = document.createElement('script');
+    script.src = `https://www.googletagmanager.com/gtag/js?id=GTM-KM7WDCDN`; // Correct - using the variable
+
+    gtag('config', GTM_ID); // Correct - using the variable
+    // ...
+  }, []);
   return (
     <div>
       <Main />
